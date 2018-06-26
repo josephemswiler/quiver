30995411758f78e30dd1d30a516f21e9eeed716b
Shane Padgett <spadgett00@gmail.com>
2018-06-26 17:25:04 -0500

    Initial project layout with NextJS

---
+++ pages/about.jsx
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout'
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Quiver</h1>
+      <p>
+        Trade equities, options and crypto assets on Robinhood and Binance from
+        a single intuitive app.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default About
